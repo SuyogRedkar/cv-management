d050e1697d3e1ef0fd3a510e29eb4322a42ea45e
SuyogRedkar <76471004+SuyogRedkar@users.noreply.github.com>
2024-07-28 23:29:30 +0530

    Update page.jsx

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -7,7 +7,7 @@ import { jwtDecode } from "jwt-decode";
 export default function ProfilePage() {
   const router = useRouter();
   const [decodedToken, setDecodedToken] = useState(null);
-  const token = localStorage.getItem('token');
+  // const token = localStorage.getItem('token');
 
   useEffect(() => {
     // const token = localStorage.getItem('token');
